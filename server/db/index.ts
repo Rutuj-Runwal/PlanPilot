26cf4f98b4b88080983c1d4f1d72eee37df0185f
Rutuj Runwal <rutujrunwal001@gmail.com>
2023-12-04 23:52:18 +0530

    Configured Database conncetion and schema

---
+++ server/db/index.ts
@@ -0,0 +1,26 @@
+import mongoose, { Schema } from "mongoose";
+
+//  Define Schemas
+const userSchema = new Schema({
+  username: String,
+  password: String,
+});
+
+const todoSchema = new Schema({
+  userId: String,
+  title: String,
+  description: String,
+  done: Boolean,
+});
+
+// Connect to DB
+if (!process.env.MONGODB_URI) {
+  throw new Error("MongoDB URI not defined in env!");
+}
+mongoose.connect(process.env.MONGODB_URI);
+// mongoose.Promise = global.Promise;
+
+// Generate Models
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+const Todo = mongoose.models.Todo || mongoose.model("Todo", todoSchema);
+export { User, Todo };
